d68369e2805d3a40ce16353ba443248f4ba569dc
Akash kumar <akash.kumar@searshc.com>
2020-08-20 15:39:09 +0530

    Initial Commit

---
+++ iOSCalculatorMADS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCalculatorMADS
+//
+//  Created by Kumar, Akash on 8/20/20.
+//  Copyright © 2020 Kumar, Akash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
